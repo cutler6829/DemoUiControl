aaea8c25b61bdfa54dbcaea3b3ebf0431a2b5c8d
cutler6829 <asplion6829@gmail.com>
2018-04-27 15:08:34 +0800

    v1.1

---
+++ app/src/main/java/com/example/user/demouicontrol/RadioButtenEx.java
@@ -0,0 +1,13 @@
+package com.example.user.demouicontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RadioButtenEx extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radiobutten_ex);
+    }
+}
