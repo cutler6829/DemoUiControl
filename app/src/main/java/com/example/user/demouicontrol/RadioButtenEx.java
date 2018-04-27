f9fb76b710f8983d1b915c839a0a8e4ac51fa6a7
cutler6829 <asplion6829@gmail.com>
2018-04-27 15:52:39 +0800

    v1.2

--- app/src/main/java/com/example/user/demouicontrol/RadioButtenEx.java
+++ app/src/main/java/com/example/user/demouicontrol/RadioButtenEx.java
@@ -2,12 +2,46 @@ package com.example.user.demouicontrol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
-public class RadioButtenEx extends AppCompatActivity {
+public class RadioButtenEx extends AppCompatActivity implements View.OnClickListener {
+    RadioButton rdbn1,rdbn2,rdbn3;
+    Button submit;
+    TextView result;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_radiobutten_ex);
+        findView();
+        setListeners();
+    }
+    void findView(){
+        rdbn1=findViewById(R.id.rdbn1);
+        rdbn2=findViewById(R.id.rdbn2);
+        rdbn3=findViewById(R.id.rdbn3);
+        submit=findViewById(R.id.submit);
+        result=findViewById(R.id.result);
+    }
+    void setListeners(){submit.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (rdbn1.isChecked()) {
+            result.setText("你選擇的結果是" + rdbn1.getText());
+        }else if(rdbn2.isChecked()){
+            result.setText("你選擇的結果是" + rdbn2.getText());
+        }else if(rdbn3.isChecked()){
+            result.setText("你選擇的結果是" + rdbn3.getText());
+        }
+
     }
 }
